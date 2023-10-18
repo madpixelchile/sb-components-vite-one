c9cc25253f9031d285c8bd60d3d6d3e27ebd5848
Juan Escudero <juanescudero@MacBook-Pro-de-Juan.local>
2023-10-18 12:25:49 -0300

    fix: Button removed to fix error

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,4 @@
-import Button from "./components/Button/Button";
-import Header from "./components/Header/Header";
 import MyLabel from "./components/MyLabel/MyLabel";
-import Page from "./components/Page/Page";
 export {
-    Button, 
-    Header,
     MyLabel,
-    Page
 }
