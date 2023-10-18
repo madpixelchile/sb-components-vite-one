b8f4be684eaec1ce9961b344a4737f2d87809c18
Juan Escudero <juanescudero@MacBook-Pro-de-Juan.local>
2023-10-18 11:57:17 -0300

    fix: Components export file

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
-import { Button, Header, MyLabel, Page } from "./stories";
+import Button from "./components/Button/Button";
+import Header from "./components/Header/Header";
+import MyLabel from "./components/MyLabel/MyLabel";
+import Page from "./components/Page/Page";
 export {
     Button, 
     Header,
