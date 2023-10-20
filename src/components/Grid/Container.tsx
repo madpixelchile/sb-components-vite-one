60f6c25dba2efa9e561c6ec394b0a61982f0891b
Juan Escudero <juanescudero@MacBook-Pro-de-Juan.local>
2023-10-20 17:50:23 -0300

    fix: container-fluid & container with other classes added

--- src/components/Grid/Container.tsx
+++ src/components/Grid/Container.tsx
@@ -1,15 +1,25 @@
 import { StyledGrid } from "./StyledGrid";
 
-export interface ContainerProps{
-    children?: React.ReactNode;
-    className?: string;
-    [key: string]: any;
+export interface ContainerProps {
+  children?: React.ReactNode;
+  className?: string;
+  [key: string]: any;
 }
 
-export const Container = ( { children = null, className = '', ...props } : ContainerProps) => {
+export const Container = ({ children = null, className = '', ...props }: ContainerProps) => {
+   // Dividir las clases en un array
+  const classes = className.split(' ');
+
+  // Verificar si "container-fluid" está presente y agregarla si no está en las clases
+  const hasContainerFluid = classes.includes('container-fluid');
+  const containerClass = hasContainerFluid ? 'container-fluid' : 'container';
+
+  // Unir las clases nuevamente en una cadena
+  const additionalClasses = classes.filter(item => item !== 'container' && item !== 'container-fluid').join(' ');
+
   return (
     <>
-        {  <StyledGrid className={`${className === 'container-fluid' ? 'container-fluid' : 'container'} ${className}`} {...props} >{ children }</StyledGrid>  }
+      {<StyledGrid className={`${containerClass} ${additionalClasses}`} {...props} >{children}</StyledGrid>}
     </>
   )
 }
