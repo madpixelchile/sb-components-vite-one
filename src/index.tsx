2d32a7a5407c58393b37a1a83beedaa17a2b8604
Juan Escudero <juanescudero@MacBook-Pro-de-Juan.local>
2023-10-20 18:54:03 -0300

    fix: maintheme exported

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import MyLabel from "./components/MyLabel/MyLabel";
 import Container from './components/Grid/Container';
 import Row from './components/Grid/Row';
 import Col from './components/Grid/Col';
+import MainTheme from './components/ThemeProvider/MainTheme';
 export {
     MyLabel,
     Container,
     Row,
-    Col
+    Col,
+    MainTheme
 }
