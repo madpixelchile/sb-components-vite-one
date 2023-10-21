8822b8bbc9f1a33c2a18f153b9dd6765a1696f24
Juan Escudero <juanescudero@MacBook-Pro-de-Juan.local>
2023-10-20 22:58:53 -0300

    fix: myLabel removed from package

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import MyLabel from "./components/MyLabel/MyLabel";
 import Container from './components/Grid/Container';
 import Row from './components/Grid/Row';
 import Col from './components/Grid/Col';
@@ -6,7 +5,6 @@ import MainTheme from './components/ThemeProvider/MainTheme';
 import Text from './components/Text/Text';
 import Heading from './components/Heading/Heading';
 export {
-    MyLabel,
     Container,
     Row,
     Col,
