5f9763c4577c790a8ba0cdd2f54e2c1f96ac955f
Juan Escudero <juanescudero@MacBook-Pro-de-Juan.local>
2023-10-21 14:28:42 -0300

    fix: Theme provider issue with interface resolved

--- src/components/ThemeProvider/MainTheme.tsx
+++ src/components/ThemeProvider/MainTheme.tsx
@@ -4,13 +4,13 @@ import { ThemeProvider } from 'styled-components';
 import { StyledThemeProvider } from './StyledThemeProvider';
 import { darkMode, lightMode } from '../../styles/theme';
 
-interface Props {
+export interface ThemeProviderProps {
   children?: React.ReactNode;
   theme?: string,
   [key: string]: any;
 }
 
-export const MainTheme = ({ children = null, theme = 'light' }:Props) => {
+export const MainTheme = ({ children = null, theme = 'light' }:ThemeProviderProps) => {
   return (
     <>
         <ThemeProvider theme={theme === 'light' ? lightMode : darkMode }>
