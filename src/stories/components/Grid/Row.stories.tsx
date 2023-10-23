197bec201e1de5d246ea71748b64dd3c9237cdfd
Juan Escudero <juanescudero@MacBook-Pro-de-Juan.local>
2023-10-22 23:45:58 -0300

    feat: Row storybook Docs added

--- src/stories/components/Grid/Row.stories.tsx
+++ src/stories/components/Grid/Row.stories.tsx
@@ -36,9 +36,23 @@ export const Basic = ({...props}:Story)=>(
     <MainTheme>
         <Container>
             <Row {...props}>
-                <Col>
+                <Col className={'col-md-6'}>
                     <Heading variant={'h1'} size={'h5'} className={'mb-2'}>Uso de row</Heading>
-                    <Text>Podremos agregarle clases de <strong>bootstrap 4</strong>, para poder manejar modificadores de flex-box.</Text>
+                    <Text className={'mb-3'}>Podremos agregarle clases de <strong>bootstrap 4</strong>, para poder manejar modificadores de flex-box.</Text>
+                    <Text><strong>.no-gutters</strong>: Al agregar esta clase a una .row, se eliminan los espacios (gutters) entre las columnas en la fila. De forma predeterminada, las filas de Bootstrap tienen espacios.</Text>
+                    <Text><strong>.align-items-start</strong>: Alinea los elementos verticalmente en la parte superior de la fila.</Text>
+                    <Text><strong>.align-items-center</strong>: Alinea los elementos verticalmente en el centro de la fila.</Text>
+                    <Text><strong>.align-items-end</strong>: Alinea los elementos verticalmente en la parte inferior de la fila.</Text>
+                    <Text><strong>.align-items-baseline</strong>: Alinea los elementos verticalmente de modo que las líneas de base del texto se alineen.</Text>
+                    <Text><strong>.align-items-stretch</strong>: Estira los elementos para ocupar la altura completa de la fila.</Text>
+                </Col>
+                <Col className={'col-md-6'}>
+                    <Text className={'mt-5'}><strong>.justify-content-start</strong>: Alinea los elementos horizontalmente al principio de la fila.</Text>
+                    <Text><strong>.justify-content-end</strong>: Alinea los elementos horizontalmente al final de la fila.</Text>
+                    <Text><strong>.justify-content-center</strong>: Centra los elementos horizontalmente en la fila.</Text>
+                    <Text><strong>.justify-content-between</strong>: Alinea los elementos de manera uniforme en la fila, con espacio entre ellos.</Text>
+                    <Text><strong>.justify-content-around</strong>: Alinea los elementos de manera uniforme en la fila, con espacio alrededor de ellos.</Text>
+                    <Text><strong>.justify-content-evenly</strong>: Alinea los elementos de manera uniforme en la fila, con espacio igual alrededor de ellos.</Text>
                 </Col>
             </Row>
         </Container>
